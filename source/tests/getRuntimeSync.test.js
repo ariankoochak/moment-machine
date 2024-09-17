efbcb16250348023a472b67fdbe2603d6f392ddc
Arian <arian.koochak@protonmail.com>
2024-09-17 11:04:34 +0330

    Update getRuntimeSync.test.js

--- source/tests/getRuntimeSync.test.js
+++ source/tests/getRuntimeSync.test.js
@@ -1,16 +1,6 @@
 const { getRuntimeSync } = require("..");
-const { inpFuncSync1, inpFuncSync2, inpFuncSync3 } = require("./testFunctions");
+const { inpFuncSync1} = require("./testFunctions");
 
 test("getRuntimeSync", async () => {
     await expect(getRuntimeSync(inpFuncSync1)).resolves.toBeGreaterThanOrEqual(0);
 });
-
-// test("getFasterFunc", () => {
-//     expect(getFasterFunc(inpFunc1, inpFunc2)).toBeInstanceOf(Function);
-// });
-
-// test("getFasterFunc", () => {
-//     expect(getFasterFunc(inpFunc1, inpFunc2, inpFunc3)).toBeInstanceOf(
-//         Function
-//     );
-// });
