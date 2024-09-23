e0ae51c3a6564ba1e3e66fd4a979cd3a06c0df7f
Arian <arian.koochak@protonmail.com>
2024-09-23 20:09:40 +0330

    fix - update getRuntimeSync test

--- source/tests/getRuntimeSync.test.js
+++ source/tests/getRuntimeSync.test.js
@@ -2,5 +2,12 @@ const { getRuntimeSync } = require("..");
 const { inpFuncSync1} = require("./testFunctions");
 
 test("getRuntimeSync", async () => {
-    await expect(getRuntimeSync(inpFuncSync1)).resolves.toBeGreaterThanOrEqual(0);
+    const test = async ()=>{
+        let res = await getRuntimeSync(inpFuncSync1);
+        if(typeof res === 'number' && isNaN(res) === false){
+            return true
+        }
+        return false
+    }
+    await expect(test()).resolves.toBe(true);
 });
