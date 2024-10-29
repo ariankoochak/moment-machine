ffc8bea9751ec6dd152094d4bf57aaa0206d2d52
Arian <arian.koochak@protonmail.com>
2024-10-29 10:49:10 +0330

    complete code compareFuncs

--- source/lib/CompareFuncs/compareFuncs.js
+++ source/lib/CompareFuncs/compareFuncs.js
@@ -1,14 +1,12 @@
 const calcRuntime = require("../../utils/calcRuntime");
 const floatFixing = require("../../utils/floatFixing");
-const {automateSort} = require('sortex')
-const getResultForMultiRuntime = require("../../utils/getResultForMultiRuntime");
-const { inpFunc1, inpFunc2, inpFunc3 } = require("../../tests/testFunctions");
+const {automateSort, getSmallest, getBiggest} = require('sortex')
 const getMultiRuntime = require("../GetMultiRunTime/getMultiRuntime");
 
 /**
- * This method executes a function several times and calculates its execution time several times and puts it in an array.
+ * With this method, you can compare the execution time of several functions and get complete information from it
  * @param {Function} inputFunctions The function we want to calculate its execution times
- * @param {Number} runtimeCount The number of times you want this function to be executed and its time taken
+ * @param {Number} runtimeCount The number of times you want input functions to be executed and its time taken
  * @returns {Object}
  */
 function compareFuncs(runtimeCount = 5, ...inputFunctions) {
@@ -23,15 +21,28 @@ function compareFuncs(runtimeCount = 5, ...inputFunctions) {
             multiRuntimes.push([func,getMultiRuntime(func,{ignoreFirstTime : true,runtimeCount : runtimeCount,moreDetails : true})]);
         }
         const sortedFirstRuntimes = automateSort(firstRuntimes,{valuePath : '1'});
+        const multiFastestRun = getSmallest(multiRuntimes,{valuePath : "1.fastestRuntimes"});
+        const multiSlowestRun = getBiggest(multiRuntimes,{valuePath : "1.slowestRuntimes"})
+        const multiFastestAverage = getSmallest(multiRuntimes,{valuePath : "1.average"})
+        const multiSlowestAverage = getBiggest(multiRuntimes,{valuePath : "1.average"})
         const rtnObj = {
-            firstRuntimes : {},
-            multiRuntimes : {},
-        }
+            firstRuntimes: {
+                fastest: getSmallest(firstRuntimes, { valuePath: "1" }),
+                slowest: getBiggest(firstRuntimes, { valuePath: "1" }),
+                rank: sortedFirstRuntimes,
+            },
+            multiRuntimes: {
+                fastestRun : [multiFastestRun[0],multiFastestRun[1].fastestRuntimes],
+                slowestRun : [multiSlowestRun[0],multiSlowestRun[1].slowestRuntimes],
+                fastestAverage : [multiFastestAverage[0],multiFastestAverage[1].average],
+                slowestAverage : [multiSlowestAverage[0],multiSlowestAverage[1].average],
+                result : multiRuntimes
+            },
+        };
+        return rtnObj
     } catch (error) {
         console.log(error);
     }
 }
-
-compareFuncs(6,inpFunc1,inpFunc2,inpFunc3)
 
 module.exports = compareFuncs;
